e6f0b86e8d9d5454313889ce10bb674530701a25
Den111222 <khodzho@gmail.com>
2023-05-19 11:08:34 +0300

    26 lesson done

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'ckeditor_uploader',
     'contact',
     'snowpenguin.django.recaptcha3',
+    'allauth',
+    'allauth.account',
 
 ]
 
@@ -96,7 +98,10 @@ DATABASES = {
     }
 }
 
-
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -126,6 +131,12 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_REDIRECT_URL = "/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 gettext = lambda s: s
 LANGUAGES = (('ru', gettext('Russian')),
